4f0baf073913d5f61182736c9ad839209be22fd3
rafaelcho <43811638+rafaelcho@users.noreply.github.com>
2019-11-03 05:24:29 +0900

    merging with mine.

--- app/src/main/java/com/example/navdrawer/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/navdrawer/ui/home/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.example.navdrawer.ui.home;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -14,22 +19,56 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.navdrawer.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class HomeFragment extends Fragment {
+    EditText nameInput;
+    EditText emailInput;
 
-    private HomeViewModel homeViewModel;
+//    Button birthButton;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+//    public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+
+    Date selectedDate;
+
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        nameInput = rootView.findViewById(R.id.nameInput);
+        emailInput = rootView.findViewById(R.id.emailInput);
+
+ //       birthButton = rootView.findViewById(R.id.birthButton);
+ //       birthButton.setOnClickListener(new View.OnClickListener() {
+ //           public void onClick(View v) {
+ //               showDateDialog();
+ //           }
+ //       });
+
+        Button saveButton = rootView.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String nameStr = nameInput.getText().toString();
+                String emailStr = emailInput.getText().toString();
+//                String birthStr = birthButton.getText().toString();
+
+                Toast.makeText(getContext(), "이름 : " + nameStr + ", 이메일 : " + emailStr, Toast.LENGTH_SHORT).show();
             }
         });
-        return root;
+
+
+        // set selected date using current date
+        Date curDate = new Date();
+ //       setSelectedDate(curDate);
+
+        return rootView;
     }
+
 }

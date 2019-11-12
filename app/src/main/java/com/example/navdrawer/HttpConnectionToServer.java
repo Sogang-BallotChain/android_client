aa41dfdbd1418c03fa38adc4fad7377669325445
pinelover <pineleaf1215@gmail.com>
2019-11-13 02:15:43 +0900

    1. create vote 구현하였습니다. 아직 후보자 목록 추가 ui를 구성 못해서 샘플 후보자들만 직접 입력해 넣었습니다.

--- app/src/main/java/com/example/navdrawer/HttpConnectionToServer.java
+++ app/src/main/java/com/example/navdrawer/HttpConnectionToServer.java
@@ -1,5 +1,7 @@
 package com.example.navdrawer;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -82,6 +84,60 @@ public class  HttpConnectionToServer {
         }
     }
     //
+    //connect to server and create Account, 1113
+    public Boolean CreateVote(JSONObject JO) {
+        try {
+            urlString = "http://www.ballotchain.net/vote/register/";
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
+            //각종세팅.
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestProperty("User-Agent","Mozilla/4.0");
+            urlConnection.setRequestMethod("POST");
+            //urlConnection.connect();
+
+
+            //post 방식
+            urlConnection.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+            System.out.println("clients sending :"+JO.toString() +"\n");
+            wr.writeBytes(JO.toString());
+            wr.flush();
+            wr.close();
+
+            // 요청 방식 구하기
+            System.out.println("getRequestMethod():" + urlConnection.getRequestMethod());
+            // 응답 콘텐츠 유형 구하기
+            System.out.println("getContentType():" + urlConnection.getContentType());
+            // 응답 코드 구하기
+            System.out.println("getResponseCode():"    + urlConnection.getResponseCode());
+            // 응답 메시지 구하기
+            System.out.println("getResponseMessage():" + urlConnection.getResponseMessage());
+            //콘텐트 요청
+            //System.out.println("getContent():" + urlConnection.getContent());
+
+            //파일 읽어 온다. GET
+            InputStream is = urlConnection.getInputStream();
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+            String result;
+            while((result = br.readLine())!= null){
+                sb.append(result + "\n");
+            }
+            result = sb.toString();
+            System.out.println("we got"+result);
+
+            return true;
+        } catch (MalformedURLException e) {
+            System.out.println("malformed url exception\n");
+            e.printStackTrace();
+            return false;
+        }catch(IOException e){
+            System.out.println("IOException\n");
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }

c4d64fde7b2d95104f21c5bdb8b2387874c0edf7
rafaelcho <43811638+rafaelcho@users.noreply.github.com>
2019-11-13 11:17:40 +0900

    프로그램 실행시 3초간 로딩 화면을 보여줌.

--- app/src/main/java/com/example/navdrawer/MainActivity.java
+++ app/src/main/java/com/example/navdrawer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.navdrawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.navdrawer.ui.create_vote.create_vote;
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        startActivity(new Intent(this,SplashActivity.class)); //new Intent는 context와 class를 인자로 받습니다. //만들어둔 SplashActivity를 호출합니다
 
         ConstraintLayout drawer = findViewById(R.id.main_layout);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
